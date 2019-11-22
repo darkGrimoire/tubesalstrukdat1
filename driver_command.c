3cb70eeff403377f5263a06f971b92c8882489eb
Faris-kun <faris.ekananda20@gmail.com>
2019-11-22 16:21:22 +0700

    Update command (90%), stackt (stackinfotype), driver_command (-5%)

---
+++ driver_command.c
@@ -0,0 +1,17 @@
+/* Driver Command ehe */
+/* By Fawwis */
+
+#include "boolean.h"
+#include "queue.h"
+#include "stackt.h"
+#include "mesinkata.h"
+#include "bangunan.h"
+#include "listlinier.h"
+#include "lib\command.h"
+#include <stdlib.h>
+
+int main(){
+    CreateFlags(&GFLAGS[0]);
+    CreateFlags(&GFLAGS[1]);
+    
+}
